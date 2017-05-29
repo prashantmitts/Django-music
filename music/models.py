8ba07db5c8863ef9715d4af266f4270416f00456
prashantmittspk <prashant.m@ikarus.ai>
2017-05-29 23:57:34 +0530

    created website

---
+++ music/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+	album = models.ForeignKey(Album,on_delete = models.CASCADE)
